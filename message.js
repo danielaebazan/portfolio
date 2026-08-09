0731377561428761b7d677153aa96a576af6093f
danielaebazan <daniela.e.bazan@hotmail.com>
2026-08-08 21:50:00 -0600

    new form

--- message.js
+++ message.js
@@ -5,7 +5,7 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    const formURL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLScCdmv56d6oDWWSmnnRkyBA3hdK9y3grlM3iraDxqg8VW4oTg/formResponse";
+    const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSdCuLWsr3oYRUd5zoO6nkMdSkZ4722QFDch58engNXeMwDNdw/formResponse";
     const formData = new FormData();
     formData.append("entry.1297566054", name);
     formData.append("entry.1072388475", email);
